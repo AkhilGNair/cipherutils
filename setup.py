4bd9c0b719d09f820c00b143efbdb23f1c6b7fc0
Akhil Nair <Akhil.nair@amey.co.uk>
2017-01-27 09:27:34 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cipherutils',
+      version='1.0',
+      description='CRC checker and fast hex2int',
+      url='https://github.com/username/repo',
+      author='Your Name',
+      author_email='email@domain.net',
+      license='MIT',
+      packages=['module'],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5'])
